eff35097a56cee601a61d22f4fddbab4ac396a3b
robert <robert.mn92@gmail.com>
2025-05-02 17:39:32 +0200

    create menu components

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold">About Little Lemon</h1>
+            <p>Our story...</p>
+        </div>
+    );
+};
+export default About;
