0efae4ae84936224442e4a9e10d7742ada399610
robert <robert.mn92@gmail.com>
2025-05-04 00:02:59 +0200

    footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,46 @@
-import React from "react"
-import small_logo from "../styles/small_logo.png"
+import React from "react";
+import small_logo from "../styles/small_logo.png";
 
 const Footer = () => {
-    return(
-        <footer>
-            <section>
-            <div>
-                <img src={small_logo} />
+    return (
+        <footer className="footer">
+            <div className="footer-columns">
+                <div className="footer-column">
+                    <img src={small_logo} alt="Little Lemon Logo" className="footer-logo" />
+                </div>
+
+                <div className="footer-column">
+                    <h3 className="footer-title">Navigation</h3>
+                    <ul className="footer-list">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/menu">Menu</a></li>
+                        <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/order-online">Order Online</a></li>
+                        <li><a href="/login">Login</a></li>
+                    </ul>
+                </div>
+
+                <div className="footer-column">
+                    <h3 className="footer-title">Contact</h3>
+                    <ul className="footer-list">
+                        <li>123 Lemon Street, Chicago</li>
+                        <li>(312) 555-1234</li>
+                        <li>info@littlelemon.com</li>
+                    </ul>
+                </div>
+
+                <div className="footer-column">
+                    <h3 className="footer-title">Social Media</h3>
+                    <ul className="footer-list">
+                        <li><a href="https://facebook.com">Facebook</a></li>
+                        <li><a href="https://instagram.com">Instagram</a></li>
+                        <li><a href="https://twitter.com">Twitter</a></li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h3>Document Navigation</h3>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
-            </ul>
-            </div>
-            <div>
-                <h3>Contact</h3>
-            <ul>
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
-            </ul>
-            </div>
-            <div>
-                <h3>Social Media Links</h3>
-            <ul>
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
-            </ul>
-            </div>
-            </section>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
