379978864932377e2d0e361b7a913978e094de48
robert <robert.mn92@gmail.com>
2025-05-13 21:03:04 +0200

    update test

--- src/timesUtils.test.js
+++ src/timesUtils.test.js
@@ -1,23 +1,36 @@
 import { initializeTimes, updateTimes } from "./utils/timeUtils";
 
+
 describe("initializeTimes", () => {
-  test("returns default list of times", () => {
+  test("returns non-empty list of times after API call", () => {
+    const mockFetchAPI = jest.fn(() => ['18:00', '20:00']);
+    global.fetchAPI = mockFetchAPI;
+
     const result = initializeTimes();
-    expect(result).toEqual(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
+    expect(result).toEqual(['18:00', '20:00']);
+    expect(mockFetchAPI).toHaveBeenCalled();
+    expect(mockFetchAPI).toHaveBeenCalledWith(expect.any(Date)); 
   });
 });
+
+
 
 describe("updateTimes", () => {
   test("returns same state on unknown action", () => {
     const initialState = ['17:00'];
     const result = updateTimes(initialState, { type: 'UNKNOWN_ACTION' });
-    expect(result).toBe(initialState); 
+    expect(result).toBe(initialState);
   });
 
   test("returns new list of times on DATE_CHANGE", () => {
-    const initialState = [];
-    const result = updateTimes(initialState, { type: 'DATE_CHANGE', payload: '2025-05-05' });
+    const mockFetchAPI = jest.fn(() => ['10:00', '12:30', '19:00']);
+    global.fetchAPI = mockFetchAPI;
 
-    expect(result).toEqual(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
+    const testDate = new Date('2025-05-05');
+    const result = updateTimes([], { type: 'DATE_CHANGE', payload: testDate });
+
+    expect(result).toEqual(['10:00', '12:30', '19:00']);
+
+    expect(mockFetchAPI).toHaveBeenCalledWith(testDate);
   });
 });
